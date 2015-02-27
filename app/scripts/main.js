b9e2ae587af9e164277cc466aab16441b5737736
Zach Perrault <zach.perrault@gmail.com>
2015-02-26 21:27:33 -0500

    Basic shell

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,23 @@
-console.log('\'Allo \'Allo!');
+var app = angular.module('app', []);
+
+app.controller('ctrl', function($scope) {
+	$scope.parse = function(input) {
+		if(!input) {
+			return = "";
+		}
+		
+		var state = 0;
+		for(var i = 0; i < input.length(); ++i) {
+			switch(state) {
+				case 0:
+
+					break;
+				case 1:
+
+					break;
+			}
+		}
+
+		return input.toUpperCase().split(" ");
+	};	
+});
